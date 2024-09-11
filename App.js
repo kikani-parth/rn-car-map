11eb48a02274eca0237730fe1b9f393f61d17892
Parth Kikani <parthkikani02@gmail.com>
2024-09-11 10:46:24 +0300

    add CarListScreen and CarMapScreen

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import CarListScreen from './src/screens/CarListScreen';
+import CarMapScreen from './src/screens/CarMapScreen';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Car List" component={CarListScreen} />
+        <Tab.Screen name="Car Map" component={CarMapScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/screens/CarListScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CarListScreen = () => {
+  return (
+    <View>
+      <Text>CarListScreen</Text>
+    </View>
+  );
+};
+
+export default CarListScreen;
