c8aa361f162c77b5ed86c7027fa0c7f6a996b0a2
Parth Kikani <parthkikani02@gmail.com>
2024-09-12 14:07:55 +0300

    show user location on the map

--- src/components/CarMap.js
+++ src/components/CarMap.js
@@ -1,26 +1,58 @@
-// CarMap.js
-
-import React from 'react';
-import { View, StyleSheet, SafeAreaView, StatusBar } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, ActivityIndicator, Text } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 const CarMap = () => {
-  const initialRegion = {
-    latitude: 37.78825, // Example: San Francisco
-    longitude: -122.4324,
-    latitudeDelta: 0.0922, // Controls the zoom level
-    longitudeDelta: 0.0421,
-  };
+  const [userLocation, setUserLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    const getLocation = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setUserLocation(currentLocation.coords);
+    };
+
+    getLocation();
+  }, []);
+
+  if (errorMsg) {
+    return (
+      <View style={styles.errorMsgContainer}>
+        <Text style={styles.errorMsg}>
+          Permission to access location was denied
+        </Text>
+      </View>
+    );
+  }
+
+  //   if (!userLocation) {
+  //     // Show a loading indicator while the location is being fetched
+  //     return (
+  //       <View style={styles.loadingContainer}>
+  //         <ActivityIndicator size="large" color="#0000ff" />
+  //       </View>
+  //     );
+  //   }
 
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} initialRegion={initialRegion}>
-        <Marker
-          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
-          title="Car Location"
-          description="This is where the car is located."
-        />
-      </MapView>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: userLocation.latitude,
+          longitude: userLocation.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+        showsUserLocation={true}
+      ></MapView>
     </View>
   );
 };
@@ -31,6 +63,20 @@ const styles = StyleSheet.create({
   },
   map: {
     ...StyleSheet.absoluteFillObject, // Fills the entire View
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  errorMsgContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  errorMsg: {
+    color: 'red',
+    fontSize: 18,
   },
 });
 
